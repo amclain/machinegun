b59344370a1c961d1b2fc50152ec5f640865d7e8
Matías Aguirre <matiasaguirre@gmail.com>
2015-10-23 19:35:13 -0300

    Follow origin style guidelines

--- lib/machinegun.rb
+++ lib/machinegun.rb
@@ -2,27 +2,29 @@ require 'rack'
 require 'filewatcher'
 
 class MachineGun
-
-  def self.run(interval = nil)
+  
+  def self.run opts = {}
     pid = start_server
-
-    FileWatcher.new("./**/*.rb").watch(interval || 0.5) do
+     
+    interval = opts[:interval] || 0.5
+     
+    FileWatcher.new("./**/*.rb").watch interval do
       Process.kill "INT", pid
       Process.wait pid
-
+      
       pid = start_server
     end
   end
-
+  
   private
-
+  
   def self.start_server
     pid = fork do
       $0 = "rack"
       Rack::Server.start
     end
-
+    
     pid
   end
-
+  
 end
