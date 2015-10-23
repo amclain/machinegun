15878df9f2fb0d351239332d65814e92b19e0971
Matías Aguirre <matiasaguirre@gmail.com>
2015-10-23 18:55:52 -0300

    Support filewatcher interval option

--- lib/machinegun.rb
+++ lib/machinegun.rb
@@ -2,27 +2,27 @@ require 'rack'
 require 'filewatcher'
 
 class MachineGun
-  
-  def self.run
+
+  def self.run(interval = nil)
     pid = start_server
-    
-    FileWatcher.new("./**/*.rb").watch do
+
+    FileWatcher.new("./**/*.rb").watch(interval || 0.5) do
       Process.kill "INT", pid
       Process.wait pid
-      
+
       pid = start_server
     end
   end
-  
+
   private
-  
+
   def self.start_server
     pid = fork do
       $0 = "rack"
       Rack::Server.start
     end
-    
+
     pid
   end
-  
+
 end
