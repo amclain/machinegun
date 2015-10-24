964c6af38125b437874c16a7755b5875dcd716a6
Alex McLain <alex@alexmclain.com>
2015-10-24 16:51:18 -0700

    Refactor to use instance methods.

--- lib/machinegun.rb
+++ lib/machinegun.rb
@@ -4,34 +4,63 @@ require 'filewatcher'
 # An automatic reloading Rack development web server for Ruby.
 class MachineGun
   
+  # Helper method to instantiate a new object and run the server.
+  # @see #run
+  def self.run *args
+    new.run *args
+  end
+  
+  def initialize
+    @running = false
+  end
+  
   # Run the automatically-reloading web server.
   # This method blocks.
   # @option opts [Numeric] :interval (0.5) Interval in seconds to scan the
   #   filesystem for changes.
-  def self.run opts = {}
+  def run opts = {}
+    @running = true
     interval = opts[:interval] || 0.5
      
-    pid = start_server
+    @pid = start_server
      
-    FileWatcher.new("./**/*.rb").watch(interval) do
-      Process.kill "INT", pid
-      Process.wait pid
-      
-      pid = start_server
+    @watcher = FileWatcher.new("./**/*.rb").tap do |watcher|
+      watcher.watch interval do
+        stop_server
+        @pid = start_server
+      end
     end
+  end
+  
+  # Stop watching for file changes and shutdown the web server.
+  def stop
+    return unless @running
+    
+    @watcher.stop
+    stop_server
+    @running = false
+  end
+  
+  # @return true if the server is running.
+  def running?
+    @running
   end
   
   private
   
   # Start the web server in a forked process.
   # @return process id
-  def self.start_server
-    pid = fork do
+  def start_server
+    fork do
       $0 = "rack"
       Rack::Server.start
     end
-    
-    pid
+  end
+  
+  # Gracefully stop the web server's forked process.
+  def stop_server
+    Process.kill "INT", @pid
+    Process.wait @pid
   end
   
 end
--- spec/machinegun_spec.rb
+++ spec/machinegun_spec.rb
@@ -4,48 +4,86 @@ describe MachineGun do
   
   # Prevent the test suite process from forking.
   def stub_fork
-    allow(MachineGun).to receive(:fork) { |&block| block.call; pid }
+    allow(subject).to receive(:fork) { |&block| block.call; pid }
+    
+    allow(Process).to receive(:kill)
+    allow(Process).to receive(:wait)
   end
   
   # Prevent the rack server from running.
   def stub_rack_server opts = {}
     called = opts.fetch :called, :once
-    Rack::Server.should_receive(:start).at_least(called)
+    ignore = opts.fetch :ignore, false
+    
+    if ignore
+      allow(Rack::Server).to receive(:start)
+    else
+      Rack::Server.should_receive(:start).at_least(called)
+    end
   end
   
   # Prevent FileWatcher from monitoring the file system.
   def stub_file_watcher opts = {}
     run_block = opts.fetch :run_block, false
+    ignore = opts.fetch :ignore, false
+    
     _interval = (respond_to?(:interval) ? interval : nil) || 0.5
     
-    FileWatcher.should_receive(:new).at_least(:once) {
-      double().tap do |d|
-        d.should_receive(:watch).with(_interval).at_least(:once) { |&block|
-          block.call if run_block
-        }
-      end
-    }
+    if ignore
+      allow(FileWatcher).to receive(:new) {
+        double(FileWatcher).tap do |d|
+          allow(d).to receive(:watch)
+          allow(d).to receive(:stop)
+        end
+      }
+    else
+      FileWatcher.should_receive(:new).at_least(:once) {
+        double(FileWatcher).tap do |d|
+          allow(d).to receive(:stop)
+          
+          d.should_receive(:watch).with(_interval).at_least(:once) { |&block|
+            block.call if run_block
+          }
+        end
+      }
+    end
   end
   
-  subject { MachineGun }
-  
   let(:pid) { 0 } # PID 0 can't be killed if Process.kill is accidentally called.
-  let(:rack_server_called) { :once }
-  let(:run_file_watcher_block) { false }
   
-  it { should respond_to :run }
+  let(:rack_server_start_called) { :once }
+  let(:ignore_rack_server) { false }
+  let(:rack_stub_opts) {{
+    ignore: ignore_rack_server,
+    called: rack_server_start_called,
+  }}
+  
+  let(:ignore_file_watcher) { false }
+  let(:run_file_watcher_block) { false }
+  let(:file_watcher_opts) {{
+    ignore: ignore_file_watcher,
+    run_block: run_file_watcher_block,
+  }}
+  
+  before {
+    stub_fork
+    stub_rack_server **rack_stub_opts
+    stub_file_watcher **file_watcher_opts
+  }
+  
+  describe "class" do
+    subject { MachineGun }
+    let(:ignore_rack_server) { true }
+    let(:ignore_file_watcher) { true }
+    
+    specify { subject.run }
+  end
   
   describe "run" do
-    before {
-      stub_fork
-      stub_rack_server called: rack_server_called
-      stub_file_watcher run_block: run_file_watcher_block
-    }
-    
     specify { subject.run }
     
     describe "auto-restart" do
-      let(:rack_server_called) { :twice }
+      let(:rack_server_start_called) { :twice }
       let(:run_file_watcher_block) { true }
       
       specify do
@@ -67,6 +105,49 @@ describe MachineGun do
         let(:interval) { nil }
         
         specify { subject.run }
+      end
+    end
+  end
+  
+  describe "running?" do
+    let(:ignore_rack_server) { true }
+    let(:ignore_file_watcher) { true }
+    
+    specify "on instantiation" do
+      subject.running?.should eq false
+    end
+    
+    specify "after run is called" do
+      subject.run
+      subject.running?.should eq true
+    end
+    
+    specify "after stop is called" do
+      subject.run
+      subject.stop
+      subject.running?.should eq false
+    end
+  end
+  
+  describe "stop" do
+    specify "running server" do
+      subject.run
+      
+      Process.should_receive(:kill).with("INT", pid).exactly(:once)
+      Process.should_receive(:wait).with(pid).exactly(:once)
+      
+      subject.stop
+    end
+    
+    describe "stopped server" do
+      let(:ignore_rack_server) { true }
+      let(:ignore_file_watcher) { true }
+      
+      specify do
+        Process.should_not_receive(:kill).with("INT", pid)
+        Process.should_not_receive(:wait).with(pid)
+        
+        subject.stop
       end
     end
   end
